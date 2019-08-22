eb50f145918599159f62fba2708dc6a75bdfee88
Neek <nikita@popovsky.pro>
2019-08-22 08:22:07 +0300

    addSource usecase

---
+++ domain/sourceType.go
@@ -0,0 +1,10 @@
+package domain
+
+type ResourceType string
+
+const(
+	Article ResourceType = "Article"
+	Book    ResourceType = "Book"
+	Video   ResourceType = "Video"
+	Audio   ResourceType = "Audio"
+)
