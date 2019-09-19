89d6e0620126a03cdd324a930ff9f42ba2b13145
nikita@popovsky.pro <QaUVTCfLdJVh340YicG!>
2019-09-19 07:12:44 +0300

    #4 | Get book info by ISBN

--- core/usecases/addSource.go
+++ core/usecases/addSource.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"image"
 	"net/http"
@@ -24,8 +25,13 @@ import (
 	"github.com/moraes/isbn"
 )
 
+const (
+	googleApiUrl      = "https://www.googleapis.com/books/v1/volumes?q=%s+isbn&fields=items/volumeInfo(title,subtitle,authors,description,industryIdentifiers,imageLinks)"
+	openLibraryApiUrl = "http://openlibrary.org/api/books?bibkeys=ISBN:%s&format=json&jscmd=data"
+)
+
 type AddSource interface {
-	Do(ctx core.ReqContext, identifier, props string, sourceType domain.SourceType) (*domain.Source, error)
+	Do(ctx core.ReqContext, identifier string, props map[string]string, sourceType domain.SourceType) (*domain.Source, error)
 }
 
 func NewAddSource(sr core.SourceRepository, log core.AppLogger, imgSaver core.ImageManager) AddSource {
@@ -39,14 +45,22 @@ type addSource struct {
 }
 
 type webPageSummary struct {
+	Title string
+	Img   image.Image
+	Desc  string
+}
+
+type bookSummary struct {
 	Title   string
-	Img     []byte
-	ImgName string
+	Isbn10  string
+	Isbn13  string
+	Img     image.Image
+	Authors []string
 	Desc    string
 }
 
 // Должен возвращать или уже созданный ранее или новый объект
-func (this *addSource) Do(ctx core.ReqContext, identifier, props string, sourceType domain.SourceType) (*domain.Source, error) {
+func (this *addSource) Do(ctx core.ReqContext, identifier string, props map[string]string, sourceType domain.SourceType) (*domain.Source, error) {
 	err := this.validate(identifier, props, sourceType)
 	if err != nil {
 		this.Log.Errorw("Not valid request",
@@ -58,38 +72,46 @@ func (this *addSource) Do(ctx core.ReqContext, identifier, props string, sourceT
 
 	s := &domain.Source{
 		Identifier: identifier,
-		Type:       sourceType,
-		Properties: props}
+		Type:       sourceType}
 
-	// Get normalized identifier
-	switch sourceType {
-	case domain.Book:
-		s.NormalizedIdentifier = this.getBookIdentifier(identifier)
-		break
-	case domain.Audio, domain.Video, domain.Article:
-		s.NormalizedIdentifier, _ = this.getLinkIdentifier(identifier)
-		break
-	default:
-		return nil, core.NewError(core.InvalidSourceType)
-	}
-
-	if s.NormalizedIdentifier == "" {
-		this.Log.Errorw("Normalized identifier is empty",
+	// Cast identifier to unified state
+	// link without protocol and www...
+	// isbn to isbn13 format
+	s.NormalizedIdentifier, err = this.normalizeIdentifier(sourceType, identifier)
+	if err != nil {
+		this.Log.Errorw("Identifier contain not valid value",
 			"ReqId", ctx.ReqId(),
+			"Error", err,
 			"Identifier", identifier)
 		return nil, core.NewError(core.InvalidRequest)
 	}
 
-	// Find exists data
+	// Find exists source by normalized identifier
 	source := this.Repo.FindByIdentifier(s.NormalizedIdentifier)
 	if source != nil {
 		return source, nil
 	}
 
+	var img image.Image
+
 	// Fetch source summary
 	switch sourceType {
 	case domain.Book:
-		// TODO: get preview
+		bookMeta, err := this.getBookMeta(s.NormalizedIdentifier)
+		if err != nil {
+			this.Log.Errorw("Book summary not parsed",
+				"ReqId", ctx.ReqId(),
+				"Error", err.Error(),
+				"Identifier", identifier)
+			return nil, err
+		}
+
+		s.Desc = bookMeta.Desc
+		s.Title = bookMeta.Title
+		img = bookMeta.Img
+		props["isbn10"] = bookMeta.Isbn10
+		props["isbn13"] = bookMeta.Isbn13
+		props["authors"] = strings.Join(bookMeta.Authors, ", ")
 		break
 	case domain.Audio, domain.Video, domain.Article:
 		pageMeta, err := this.getWebPageMeta(identifier)
@@ -100,27 +122,70 @@ func (this *addSource) Do(ctx core.ReqContext, identifier, props string, sourceT
 				"Identifier", identifier)
 			return nil, err
 		}
+
 		s.Title = pageMeta.Title
 		s.Desc = pageMeta.Desc
-
-		if len(pageMeta.Img) > 0 {
-			err = this.ImageManager.Save(pageMeta.Img, pageMeta.ImgName)
-			if err != nil {
-				s.Img = ""
-				this.Log.Errorw("Fail to save image",
-					"ReqId", ctx.ReqId(),
-					"Error", err.Error(),
-					"Identifier", identifier)
-			} else {
-				s.Img = pageMeta.ImgName
-			}
-		}
+		img = pageMeta.Img
 	default:
 		return nil, core.NewError(core.InvalidSourceType)
 	}
 
+	// Resize and save image
+	if img != nil {
+		s.Img, err = this.resizeAndSaveImage(img)
+
+		if err != nil {
+			s.Img = ""
+			this.Log.Errorw("Fail to save image",
+				"ReqId", ctx.ReqId(),
+				"Error", err.Error(),
+				"Identifier", identifier)
+		}
+	}
+
+	p, err := json.Marshal(props)
+	if err != nil {
+		return nil, core.NewError(core.InvalidProperties)
+	}
+
+	s.Properties = string(p)
 	s = this.Repo.GetOrAddByIdentifier(s)
 	return s, nil
+}
+
+func (this *addSource) resizeAndSaveImage(img image.Image) (string, error) {
+	resized, err := this.resizeImage(200, 0, img)
+	if err != nil {
+		return "", err
+	}
+
+	name := this.generateFileName("jpg")
+	err = this.ImageManager.Save(resized, name)
+	if err != nil {
+		return "", err
+	}
+
+	return name, nil
+}
+
+func (this *addSource) normalizeIdentifier(sourceType domain.SourceType, identifier string) (string, error) {
+	switch sourceType {
+	case domain.Book:
+		return this.getBookIdentifier(identifier)
+	case domain.Audio, domain.Video, domain.Article:
+		return this.getLinkIdentifier(identifier)
+	default:
+		return "", core.NewError(core.InvalidSourceType)
+	}
+}
+
+func (this *addSource) getImage(uri string) (image.Image, error) {
+	img, err := this.getImageByUrl(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
 }
 
 func (this *addSource) getWebPageMeta(uri string) (*webPageSummary, error) {
@@ -159,6 +224,133 @@ func (this *addSource) getWebPageMeta(uri string) (*webPageSummary, error) {
 	}
 
 	return nil, core.NewError(core.InaccessibleWebPage)
+}
+
+func (this *addSource) getBookMeta(isbn13 string) (*bookSummary, error) {
+	summary, err := this.getBookMetaFromGoogle(isbn13)
+	if err == nil {
+		return summary, err
+	}
+
+	return this.getBookMetaFromOpenLibrary(isbn13)
+}
+
+func (this *addSource) getBookMetaFromGoogle(isbn13 string) (*bookSummary, error) {
+	uri := fmt.Sprintf(googleApiUrl, isbn13)
+	res, err := http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Google Book api return status [%s]", res.Status)
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	data := new(googleBooksSearch)
+	err = decoder.Decode(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// find suitable book info
+	var summary *bookSummary
+	for i := 0; i < len(data.Items); i++ {
+		item := data.Items[i]
+		if item.VolumeInfo != nil && item.VolumeInfo.IndustryIdentifiers != nil {
+			for j := 0; j < len(item.VolumeInfo.IndustryIdentifiers); j++ {
+				id := item.VolumeInfo.IndustryIdentifiers[j]
+				if id.Identifier == isbn13 {
+					summary = &bookSummary{
+						Isbn13:  id.Identifier,
+						Title:   item.VolumeInfo.Title,
+						Authors: item.VolumeInfo.Authors,
+						Desc:    item.VolumeInfo.Description,
+					}
+
+					if item.VolumeInfo.ImageLinks.Thumbnail != "" {
+						summary.Img, err = this.getImage(item.VolumeInfo.ImageLinks.Thumbnail)
+						if err == nil {
+							this.Log.Errorw("Fail to download image from",
+								"Url", item.VolumeInfo.ImageLinks.Thumbnail)
+						}
+					}
+					break
+				}
+			}
+
+			if summary != nil {
+				for k := 0; k < len(item.VolumeInfo.IndustryIdentifiers); k++ {
+					id := item.VolumeInfo.IndustryIdentifiers[k]
+					if id.TypeName == "ISBN_10" {
+						summary.Isbn10 = id.Identifier
+					}
+				}
+				break
+			}
+		}
+	}
+
+	if summary != nil {
+		return summary, nil
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+	return nil, fmt.Errorf("Fail to parse book info from response: %s", buf.String())
+}
+
+func (this *addSource) getBookMetaFromOpenLibrary(isbn13 string) (*bookSummary, error) {
+	uri := fmt.Sprintf(openLibraryApiUrl, isbn13)
+	res, err := http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("OpenLibrary Book api return status [%s]", res.Status)
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	data := new(map[string]openLibraryBook)
+	err = decoder.Decode(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var summary *bookSummary
+	for _, value := range *data {
+		authors := []string{}
+		for _, author := range value.Authors {
+			authors = append(authors, author.Name)
+		}
+		summary = &bookSummary{
+			Isbn13:  value.Identifiers.Isbn13[0],
+			Isbn10:  value.Identifiers.Isbn10[0],
+			Title:   value.Title,
+			Authors: authors,
+			Desc:    "",
+		}
+
+		if value.Cover.Large != "" {
+			summary.Img, err = this.getImage(value.Cover.Large)
+			if err == nil {
+				this.Log.Errorw("Fail to download image from",
+					"Url", value.Cover.Large)
+			}
+		}
+		break
+	}
+
+	if summary != nil {
+		return summary, nil
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+	return nil, fmt.Errorf("Fail to parse book info from response: %s", buf.String())
 }
 
 func (this *addSource) decodeIDN(uri string) string {
@@ -226,13 +418,12 @@ func (this *addSource) getWebPageSummaryFromMeta(selections *goquery.Selection, 
 				meta.Desc = content
 				break
 			case "image", "image:src":
-				if len(meta.Img) == 0 {
-					img, err := this.getImageByUrl(content)
-					if err != nil {
-						break
+				if meta.Img == nil {
+					meta.Img, _ = this.getImage(content)
+					if meta.Img == nil {
+						this.Log.Errorw("Fail to download image from",
+							"Url", content)
 					}
-					meta.Img, _ = this.resizeImage(160, 0, img)
-					meta.ImgName = this.generateFileName("jpg")
 				}
 				break
 			}
@@ -320,17 +511,21 @@ func (this *addSource) getLinkIdentifier(identifier string) (string, error) {
 	return fmt.Sprintf("%s%s%s", host, path, query), nil
 }
 
-func (this *addSource) getBookIdentifier(identifier string) string {
+func (this *addSource) getBookIdentifier(identifier string) (string, error) {
 	b := strings.Replace(identifier, "-", "", -1)
+	if !isbn.Validate(b) {
+		return "", fmt.Errorf("Isbn is not valid: %s", b)
+	}
+
 	if len(b) == 10 {
-		identifier, _ = isbn.To13(b)
+		return isbn.To13(b)
 	} else {
 		identifier = b
 	}
-	return identifier
+	return identifier, nil
 }
 
-func (this *addSource) validate(identifier, props string, sourceType domain.SourceType) error {
+func (this *addSource) validate(identifier string, props map[string]string, sourceType domain.SourceType) error {
 
 	if sourceType != domain.Book {
 		u, err := url.Parse(identifier)
@@ -352,8 +547,57 @@ func (this *addSource) validate(identifier, props string, sourceType domain.Sour
 		}
 	}
 
-	if ok := core.IsJson(props); !ok {
-		return core.NewError(core.InvalidProperties)
-	}
 	return nil
+}
+
+// google api types
+type googleBooksSearch struct {
+	TotalItems int    `json:"totalItems"`
+	Items      []Item `json:"items"`
+}
+
+type industryIdentifier struct {
+	TypeName   string `json:"type"`
+	Identifier string `json:"identifier"`
+}
+
+type imageLinks struct {
+	SmallThumbnail string `json:"smallThumbnail"`
+	Thumbnail      string `json:"thumbnail"`
+}
+
+type VolumeInfo struct {
+	Title               string               `json:"title"`
+	Authors             []string             `json:"authors"`
+	Oublisher           string               `json:"publisher"`
+	Description         string               `json:"description"`
+	IndustryIdentifiers []industryIdentifier `json:"industryIdentifiers"`
+	ImageLinks          imageLinks           `json:"imageLinks"`
+	Language            string               `json:"language"`
+}
+
+type Item struct {
+	VolumeInfo *VolumeInfo `json:"volumeInfo"`
+}
+
+// Open library api types
+
+type openLibraryBook struct {
+	Title       string      `json:"title"`
+	Cover       cover       `json:"cover"`
+	Authors     []author    `json:"authors"`
+	Identifiers identifiers `json:"identifiers"`
+}
+
+type cover struct {
+	Large string `json:"large"`
+}
+
+type author struct {
+	Name string `json:"name"`
+}
+
+type identifiers struct {
+	Isbn13 []string `json:"isbn_13"`
+	Isbn10 []string `json:"isbn_10"`
 }
