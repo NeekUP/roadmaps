c7f1ff57fd65949c67047b854ac1eb045c290283
Neek <nikita@popovsky.pro>
2020-05-17 12:09:39 +0300

    topic/search to search

--- api/topicsapi.go
+++ api/topicsapi.go
@@ -181,49 +181,6 @@ func GetTopic(getTopic usecases.GetTopic, log core.AppLogger) func(w http.Respon
 	}
 }
 
-type searchTopicReq struct {
-	Tags  []string `json:"tags"`
-	Query string   `json:"query"`
-	Count int      `json:"count"`
-}
-
-func (req *searchTopicReq) Sanitize() {
-	req.Query = StrictSanitize(req.Query)
-	for i, tag := range req.Tags {
-		req.Tags[i] = StrictSanitize(tag)
-	}
-}
-
-type searchTopicRes struct {
-	Str    string  `json:"query"`
-	Result []topic `json:"result"`
-}
-
-func SearchTopic(searchTopic usecases.SearchTopic, log core.AppLogger) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		data := new(searchTopicReq)
-		err := decoder.Decode(data)
-		defer r.Body.Close()
-
-		if err != nil {
-			statusResponse(w, &status{Code: http.StatusBadRequest})
-			return
-		}
-		if data.Tags == nil {
-			data.Tags = []string{}
-		}
-
-		data.Sanitize()
-		topics := searchTopic.Do(infrastructure.NewContext(r.Context()), data.Query, data.Tags, data.Count)
-		dtos := make([]topic, len(topics))
-		for i, t := range topics {
-			dtos[i] = *NewTopicDto(&t)
-		}
-		valueResponse(w, &searchTopicRes{Result: dtos, Str: data.Query})
-	}
-}
-
 type addTopicTagReq struct {
 	TopicName string `json:"topicname"`
 	TagName   string `json:"tagname"`
